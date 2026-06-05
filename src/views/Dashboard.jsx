3c7999af6a41a2acc468110576993b4ade73569b
Ernesto Inglés <ernestoingles.dev@gmail.com>
2026-06-04 20:34:35 -0600

    feat: implementación de DashBoard KPI 1 y 2

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -14,6 +14,17 @@ const Dashboard = () => {
                     allowFullScreen="true"
                 ></iframe>
             </Card>
+            <br />
+            {/* DashBoard Ernesto Sevilla */}
+            <Card style={{ height: 600 }}>
+                <iframe
+                    title="estadisticas"
+                    width="100%"
+                    height="100%"
+                    src="https://app.powerbi.com/view?r=eyJrIjoiZmI4ZTg2ZmYtZmEyNS00ODBiLTgzOTEtMDdiNDA1Y2JlZjA1IiwidCI6ImU0NzY0NmZlLWRhMjctNDUxOC04NDM2LTVmOGIxNThiYTEyNyIsImMiOjR9"
+                    allowFullScreen="true"
+                ></iframe>
+            </Card>
         </Container>
     );
 };
