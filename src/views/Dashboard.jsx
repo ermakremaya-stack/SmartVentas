79cd2ce5bfd989752b5e0bc6d4450e43439bbe0a
Creivin <estudiantekreivin@gmail.com>
2026-06-09 09:11:48 -0600

    Dashbord numero 4

--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -25,6 +25,18 @@ const Dashboard = () => {
                     allowFullScreen="true"
                 ></iframe>
             </Card>
+
+            <br />
+            {/* DashBoard 4 */}
+            <Card style={{ height: 600 }}>
+                <iframe
+                    title="estadisticas"
+                    width="100%"
+                    height="100%"
+                    src="https://app.powerbi.com/view?r=eyJrIjoiNjU0YjA5NDQtY2Q4MC00NTgzLWI1ZjctOTI2YWFjODNkYTE0IiwidCI6ImU0NzY0NmZlLWRhMjctNDUxOC04NDM2LTVmOGIxNThiYTEyNyIsImMiOjR9"
+                    allowFullScreen="true"
+                ></iframe>
+            </Card>
         </Container>
     );
 };
